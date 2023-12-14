000d4409d845b77e7ba714cfb48736aff1288d79
Ivan <ivanpiccioni.work@gmail.com>
2023-12-14 10:54:34 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
         Unicode unicodeCharacter = new Unicode("Hello, World!", 7);
-        String result = unicodeCharacter.getUnicodeCharacter();
-        System.out.println(result);
+        System.out.println(unicodeCharacter.getUnicodeCharacter());
     }
 }
--- src/Unicode.java
+++ src/Unicode.java
@@ -1,4 +1,5 @@
 public class Unicode {
+
     private String inputString;
     private int position;
 
@@ -8,20 +9,12 @@ public class Unicode {
     }
 
     public String getUnicodeCharacter() {
-        try {
-            // Controlla se l'indice è valido
-            if (position >= 0 && position < inputString.length()) {
-                // Ottieni il carattere in Unicode
-                char character = inputString.charAt(position);
-                return "Il carattere in Unicode all'indice " + position + " è: " + Integer.toHexString(character | 0x10000).substring(1);
-            } else {
-                // Indice non valido
-                return "Errore: Indice non valido.";
-            }
-        } catch (Exception e) {
-            // Gestisci altre eccezioni
-            return "Errore: " + e.getMessage();
+        if (position >= 0 && position < inputString.length()) {
+            char character = inputString.charAt(position);
+            return "Unicode character at position " + position + ": " + (int) character;
+        } else {
+            return "Invalid position. Please provide a valid index within the string length.";
         }
     }
-}
+   }
 
